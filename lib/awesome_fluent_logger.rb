05b29e501643388d7bcee9d58f172650e57dd35a
windyakin <windyakin@pepabo.com>
2021-01-25 15:47:50 +0900

    Fix initialize kvargs for Ruby 3.0

--- lib/awesome_fluent_logger.rb
+++ lib/awesome_fluent_logger.rb
@@ -3,8 +3,8 @@ require 'awesome_fluent_logger/logger'
 require 'awesome_fluent_logger/formatter'
 
 module AwesomeFluentLogger
-  def self.new(*args)
-    AwesomeFluentLogger::Logger.new(*args)
+  def self.new(**args)
+    AwesomeFluentLogger::Logger.new(**args)
   end
   class FluentConnectionError < StandardError; end
 end
